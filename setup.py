dcf87c938bfeccda8a64e65c813c71f067576890
Gorfiend <Gorfiend@gmail.com>
2021-03-22 11:34:39 -0400

    So more rebranding. Will need to finish up the setup and such

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ import os
 from distutils.core import setup, Extension
 
 setup(
-    name = 'pyanidb',
-    version = '0.2.1',
-    author = 'Vegard Storheil Eriksen',
-    author_email = 'zyp@jvnv.net',
-    url = 'http://redmine.jvnv.net/projects/pyanidb/',
-    packages = ['pyanidb'],
-    scripts = ['anidb'])
+    name = 'tsubodb',
+    version = '0.9.0',
+    author = 'Gorfiend',
+    url = 'TODO',
+    packages = ['tsubodb'],
+    scripts = ['tsubodb'])
